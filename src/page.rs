aa8cf3d6bee20ef10c244013a9d982712e7b2fb2
Jaffa Planquart <jaffa@planquart.fr>
2025-01-12 12:25:21 +0100

    feat(obsidian): Handling pages the obsidian way

--- src/page.rs
+++ src/page.rs
@@ -21,14 +21,22 @@ impl Page {
     }
 
     pub fn write(&mut self) -> Result<()> {
-        let mut file = std::fs::File::create(&self.path)?;
-        write!(file, "{}", self.content)?;
+        if let Some(parent) = self.path.parent() {
+            if !parent.exists() {
+                std::fs::create_dir_all(parent)
+                    .with_context(|| format!("creating dir {:?}", parent))?;
+            }
+        }
+
+        let mut file = std::fs::File::create(&self.path)
+            .with_context(|| format!("creating file {:?}", self.path))?;
+        write!(file, "{}", self.content)
+            .with_context(|| format!("writing file {:?}", self.path))?;
         Ok(())
     }
 
     pub fn push_content<C: Display>(&mut self, content: C) {
-        self.content.content.push(format!("- {}", content))
-
+        self.content.content.push(format!("{}", content))
     }
 
     pub fn push_metadata<M: Into<Metadata>>(&mut self, metadata: M) {
@@ -67,16 +75,19 @@ pub struct Content {
 
 impl Display for Content {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "---")?;
         for line in &self.metadata {
             writeln!(f, "{}", line)?;
         }
-        writeln!(f)?;
+        writeln!(f, "---")?;
 
-        for (index, line) in self.content.iter().enumerate() {
-            if index == 0 && line != "-" {
-                writeln!(f, "-")?;
+        if let Some(line) = self.content.first() {
+            if !line.is_empty() {
+                writeln!(f)?;
             }
+        }
 
+        for line in &self.content {
             writeln!(f, "{}", line)?;
         }
         Ok(())
@@ -89,26 +100,23 @@ impl FromStr for Content {
     fn from_str(string: &str) -> Result<Self> {
         let mut page = Content::default();
         let mut read_content = false;
-        let mut content = String::new();
+        let mut read_metadata = false;
 
         for line in string.lines() {
-            if !read_content {
-                if line.starts_with("-") {
+            if !read_metadata && !read_content {
+                if line == "---" {
+                    read_metadata = true;
+                }
+            } else if read_metadata {
+                if line == "---" {
                     read_content = true;
-                    content = line.to_owned();
-                } else if !line.is_empty() {
+                    read_metadata = false;
+                } else {
                     page.metadata.push(line.parse()?);
                 }
-            } else if line.starts_with("- ") {
-                page.content.push(content);
-                content = line.to_owned();
             } else {
-                content.push('\n');
-                content.push_str(line);
+                page.content.push(line.to_owned())
             }
-        }
-        if read_content {
-            page.content.push(content);
         }
 
         Ok(page)
@@ -146,22 +154,19 @@ mod tests {
         let temp_dir = assert_fs::TempDir::new()?;
         let file = temp_dir.child("page.md");
 
-        let metadata = indoc! {r#"
-            month:: [[2024/September]]
-            filters:: {"month" false}
-        "#};
+        let metadata = r#"month: "[[2024/September]]""#;
         let content = indoc! {"
             - TODO Something
             - DONE Something else
-              :LOGBOOK:
-              :END:
             - One other thing
         "};
 
         file.write_str(
             formatdoc!(
                 "
+            ---
             {metadata}
+            ---
             {content}"
             )
             .as_str(),
@@ -170,17 +175,19 @@ mod tests {
         let mut page: Page = file.path().try_into()?;
         page.write()?;
         file.assert(formatdoc! {"
+            ---
             {metadata}
-            -
+            ---
+
             {content}"});
 
         let second_file = temp_dir.child("another page.md");
         second_file.write_str(indoc! {r#"
-            filters:: {"month" false}
-            week:: yes
+            ---
+            week: "yes"
+            ---
 
-            -
-            - DONE Something else
+            - DONE Something
             - TODO Something
             - One other thing
         "#})?;
@@ -190,17 +197,15 @@ mod tests {
         final_page.write()?;
 
         second_file.assert(indoc! {r#"
-            filters:: {"month" false}
-            week:: yes
-            month:: [[2024/September]]
+            ---
+            week: "yes"
+            month: "[[2024/September]]"
+            ---
 
-            -
-            - DONE Something else
+            - DONE Something
             - TODO Something
             - One other thing
             - DONE Something else
-              :LOGBOOK:
-              :END:
         "#});
 
         Ok(())
