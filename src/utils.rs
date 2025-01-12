fb1a86a722751efdd8c20b388a24b76e093fffd7
Jaffa Planquart <jaffa@planquart.fr>
2025-01-12 12:14:44 +0100

    feat(obsidian): Remove methods to get of journal path

--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,7 @@ impl<T: JournalName> ToLink for T {
 }
 
 #[derive(Debug, Clone, derive_more::Display)]
-#[display("{{{{embed {link}}}}}")]
+#[display("!{link}")]
 pub struct Embedded {
     pub link: Link,
 }
@@ -35,15 +35,11 @@ impl ToEmbedded for Link {
 
 pub trait JournalName {
     fn to_journal_name(&self) -> String;
-    fn to_journal_path_name(&self) -> String;
 }
 
 impl JournalName for IsoWeek {
     fn to_journal_name(&self) -> String {
         format!("{:04}/Week {:02}", self.year(), self.week())
-    }
-    fn to_journal_path_name(&self) -> String {
-        format!("{:04}___Week {:02}.md", self.year(), self.week())
     }
 }
 
@@ -51,31 +47,16 @@ impl JournalName for NaiveDate {
     fn to_journal_name(&self) -> String {
         format!("{:04}-{:02}-{:02}", self.year(), self.month(), self.day())
     }
-    fn to_journal_path_name(&self) -> String {
-        format!(
-            "{:04}_{:02}_{:02}.md",
-            self.year(),
-            self.month(),
-            self.day()
-        )
-    }
 }
 
 impl JournalName for Month {
     fn to_journal_name(&self) -> String {
         format!("{}/{}", self.year(), self.name())
     }
-    fn to_journal_path_name(&self) -> String {
-        format!("{}___{}.md", self.year(), self.name())
-    }
 }
 
 impl JournalName for Year {
     fn to_journal_name(&self) -> String {
         self.to_string()
-    }
-
-    fn to_journal_path_name(&self) -> String {
-        format!("{}.md", self)
     }
 }
