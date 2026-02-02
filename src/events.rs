6eb5661ee49e17e0b16bf8274e430133a8cf4613
Jaffa Planquart <jaffa@planquart.fr>
2026-02-02 11:48:35 +0100

    feat: Add RelativeMonthly recurrence

--- src/events.rs
+++ src/events.rs
@@ -1,3 +1,4 @@
+use crate::date_utils::Month;
 use crate::page::content::CodeBlock;
 use anyhow::{Error, Result};
 use chrono::{Datelike, NaiveDate, Weekday};
@@ -36,22 +37,32 @@ impl TryFrom<SerdeEvent> for Event {
                 if !event.yeardays.is_empty() {
                     anyhow::bail!("`yeardays` not allowed for weekly recurrence");
                 }
+                if event.weekdays.is_empty() {
+                    anyhow::bail!("`weekdays` must be specified");
+                }
                 Recurrence::Weekly(event.weekdays)
             }
             Frequency::Monthly => {
-                if !event.weekdays.is_empty() {
-                    anyhow::bail!("`weekdays` not allowed for monthly recurrence");
-                }
                 if !event.yeardays.is_empty() {
                     anyhow::bail!("`yeardays` not allowed for monthly recurrence");
                 }
-                Recurrence::Monthly(
-                    event
-                        .monthdays
-                        .into_iter()
-                        .map(Monthday::try_from)
-                        .collect::<Result<Vec<_>>>()?,
-                )
+                if event.weekdays.is_empty() {
+                    if event.monthdays.is_empty() {
+                        anyhow::bail!("Either `monthdays` or `weekdays` must be specified");
+                    }
+                    Recurrence::Monthly(
+                        event
+                            .monthdays
+                            .into_iter()
+                            .map(Monthday::try_from)
+                            .collect::<Result<Vec<_>>>()?,
+                    )
+                } else {
+                    Recurrence::RelativeMonthly(
+                        event.weekdays,
+                        event.index.unwrap_or(WeekIndex::First),
+                    )
+                }
             }
             Frequency::Yearly => {
                 if !event.weekdays.is_empty() {
@@ -59,6 +70,9 @@ impl TryFrom<SerdeEvent> for Event {
                 }
                 if !event.monthdays.is_empty() {
                     anyhow::bail!("`monthdays` not allowed for yearly recurrence");
+                }
+                if event.yeardays.is_empty() {
+                    anyhow::bail!("`yeardays` must be specified");
                 }
                 Recurrence::Yearly(
                     event
@@ -89,7 +103,8 @@ pub struct SerdeEvent {
     monthdays: Vec<u32>,
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     yeardays: Vec<u32>,
-    pub content: String,
+    index: Option<WeekIndex>,
+    content: String,
     #[serde(flatten)]
     validity: DateRange,
     #[serde(default)]
@@ -104,6 +119,7 @@ impl From<Event> for SerdeEvent {
             weekdays: Default::default(),
             monthdays: Default::default(),
             yeardays: Default::default(),
+            index: None,
             content: event.content,
             validity: event.validity,
             exceptions: event.exceptions,
@@ -130,18 +146,20 @@ impl From<Event> for SerdeEvent {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Recurrence {
     Daily,
     /// Weekly every Weekday
     Weekly(Vec<Weekday>),
     /// Monthly each Nth day, starting from 1
     Monthly(Vec<Monthday>),
+    /// Relative monthly, e.g. each First Monday
+    RelativeMonthly(Vec<Weekday>, WeekIndex),
     /// Yearly each Nth day, starting from 1
     Yearly(Vec<Yearday>),
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Monthday(u32);
 
 impl TryFrom<u32> for Monthday {
@@ -156,13 +174,7 @@ impl TryFrom<u32> for Monthday {
     }
 }
 
-impl PartialEq<u32> for Monthday {
-    fn eq(&self, other: &u32) -> bool {
-        self.0 == *other
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Yearday(u32);
 
 impl TryFrom<u32> for Yearday {
@@ -177,20 +189,33 @@ impl TryFrom<u32> for Yearday {
     }
 }
 
-impl PartialEq<u32> for Yearday {
-    fn eq(&self, other: &u32) -> bool {
-        self.0 == *other
-    }
-}
-
 impl Recurrence {
     pub fn matches(&self, date: NaiveDate) -> bool {
         use Recurrence::*;
         match self {
             Daily => true,
-            Weekly(weekdays) => weekdays.iter().any(|day| *day == date.weekday()),
-            Monthly(monthdays) => monthdays.iter().any(|day| *day == date.day()),
-            Yearly(yeardays) => yeardays.iter().any(|day| *day == date.ordinal()),
+            Weekly(weekdays) => weekdays.contains(&date.weekday()),
+            Monthly(monthdays) => monthdays.contains(&Monthday(date.day())),
+            Yearly(yeardays) => yeardays.contains(&Yearday(date.ordinal())),
+
+            RelativeMonthly(weekdays, index) => {
+                if weekdays.contains(&date.weekday()) {
+                    let monthday0 = date.day0();
+                    let week_index = monthday0 / 7;
+                    let month = Month::from(date);
+                    let from_last_index = (month.num_days() - date.day()) / 7;
+
+                    match index {
+                        WeekIndex::First => week_index == 0,
+                        WeekIndex::Second => week_index == 1,
+                        WeekIndex::Third => week_index == 2,
+                        WeekIndex::Fourth => week_index == 3,
+                        WeekIndex::Last => from_last_index == 0,
+                    }
+                } else {
+                    false
+                }
+            }
         }
     }
 }
@@ -202,6 +227,16 @@ pub enum Frequency {
     Weekly,
     Monthly,
     Yearly,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, derive_more::IsVariant)]
+#[serde(rename_all = "snake_case")]
+pub enum WeekIndex {
+    First,
+    Second,
+    Third,
+    Fourth,
+    Last,
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
@@ -250,65 +285,274 @@ impl TryFrom<&CodeBlock> for Event {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use claim::{assert_err, assert_ok};
 
-    mod event_from_code_block {
-        use super::*;
-        use indoc::indoc;
-
-        fn block(content: &str) -> CodeBlock {
-            CodeBlock {
-                kind: "toml".to_string(),
-                code: content.to_string(),
-            }
+    fn block(content: &str) -> CodeBlock {
+        CodeBlock {
+            kind: "toml".to_owned(),
+            code: content.to_owned(),
         }
+    }
 
-        #[test]
-        fn not_a_toml_block() {
-            let block = CodeBlock {
-                kind: "foo".to_string(),
-                code: "".to_string(),
-            };
-            assert!(Event::try_from(&block).is_err());
-        }
+    #[test]
+    fn try_from_not_a_toml_block() {
+        assert_err!(Event::try_from(&CodeBlock {
+            kind: "foo".to_owned(),
+            code: "".to_owned(),
+        }));
+    }
 
-        #[test]
-        fn no_frequency() {
-            let block = block("");
-            assert!(Event::try_from(&block).is_err());
-        }
+    #[test]
+    fn no_frequency() {
+        assert_err!(Event::try_from(&block(r#"content = "foo""#)));
+    }
 
-        #[test]
-        fn no_content() {
-            let block = block(indoc! {r#"
-                frequency = daily
-            "#});
-            assert!(Event::try_from(&block).is_err());
-        }
+    #[test]
+    fn no_content() {
+        assert_err!(Event::try_from(&block(r#"frequency = "daily""#)));
+    }
 
-        #[test]
-        fn simple() -> Result<()> {
-            let block = block(indoc! {r#"
+    #[test]
+    fn simple() -> Result<()> {
+        let block = block(
+            r#"
                 frequency = "daily"
                 content = "Foo"
-            "#});
-            let event = Event::try_from(&block)?;
-            assert!(matches!(event.recurrence, Recurrence::Daily));
-            assert_eq!("Foo", event.content);
-            Ok(())
-        }
+            "#,
+        );
+        let event = Event::try_from(&block)?;
+        assert!(matches!(event.recurrence, Recurrence::Daily));
+        assert_eq!("Foo", event.content);
+        Ok(())
+    }
 
-        #[test]
-        fn dates() -> Result<()> {
-            let block = block(indoc! {r#"
+    #[test]
+    fn dates() {
+        let event = assert_ok!(Event::try_from(&block(
+            r#"
                 frequency = "daily"
                 content = "Foo"
                 from = "2025-01-01"
                 to = "2025-01-31"
-            "#});
-            let event = Event::try_from(&block)?;
-            assert_eq!("2025-01-01".parse().ok(), event.validity.from);
-            assert_eq!("2025-01-31".parse().ok(), event.validity.to);
-            Ok(())
+            "#,
+        )));
+        assert_eq!("2025-01-01".parse().ok(), event.validity.from);
+        assert_eq!("2025-01-31".parse().ok(), event.validity.to);
+    }
+
+    mod daily {
+        use super::*;
+
+        #[test]
+        fn daily() {
+            assert_ok!(Event::try_from(&block(
+                r#"
+                frequency = "daily"
+                content = "Daily"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn daily_weekdays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "daily"
+                weekdays = ["Monday"]
+                content = "Daily"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn daily_monthdays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "daily"
+                monthdays = [1]
+                content = "Daily"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn daily_yeardays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "daily"
+                yeardays = [1]
+                content = "Daily"
+            "#,
+            )));
+        }
+    }
+
+    mod weekly {
+        use super::*;
+
+        #[test]
+        fn weekly_weekdays() {
+            let event = assert_ok!(Event::try_from(&block(
+                r#"
+                frequency = "weekly"
+                weekdays = ["Monday"]
+                content = "Weekly"
+            "#,
+            )));
+
+            assert_eq!(Recurrence::Weekly(vec![Weekday::Mon]), event.recurrence);
+        }
+
+        #[test]
+        fn weekly_empty_weekdays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "weekly"
+                content = "Weekly"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn weekly_monthdays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "weekly"
+                monthdays = [1]
+                content = "Weekly"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn weekly_yeardays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "weekly"
+                yeardays = [1]
+                content = "Weekly"
+            "#,
+            )));
+        }
+    }
+
+    mod monthly {
+        use super::*;
+
+        #[test]
+        fn monthly_unspecified() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "monthly"
+                content = "Weekly"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn monthly_weekdays() {
+            let event = assert_ok!(Event::try_from(&block(
+                r#"
+                frequency = "monthly"
+                weekdays = ["Monday"]
+                content = "Weekly"
+            "#,
+            )));
+
+            assert_eq!(
+                Recurrence::RelativeMonthly(vec![Weekday::Mon], WeekIndex::First),
+                event.recurrence
+            );
+        }
+
+        #[test]
+        fn monthly_weekdays_index() {
+            let event = assert_ok!(Event::try_from(&block(
+                r#"
+                frequency = "monthly"
+                weekdays = ["Sunday", "Friday"]
+                index = "last"
+                content = "Weekly"
+            "#,
+            )));
+
+            assert_eq!(
+                Recurrence::RelativeMonthly(vec![Weekday::Sun, Weekday::Fri], WeekIndex::Last),
+                event.recurrence
+            );
+        }
+
+        #[test]
+        fn monthly_monthdays() {
+            let event = assert_ok!(Event::try_from(&block(
+                r#"
+                frequency = "monthly"
+                monthdays = [1]
+                content = "Weekly"
+            "#,
+            )));
+
+            assert_eq!(Recurrence::Monthly(vec![Monthday(1)]), event.recurrence);
+        }
+
+        #[test]
+        fn monthly_yeardays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "monthly"
+                yeardays = [1]
+                content = "Weekly"
+            "#,
+            )));
+        }
+    }
+
+    mod yearly {
+        use super::*;
+
+        #[test]
+        fn yearly_yeardays() {
+            let event = assert_ok!(Event::try_from(&block(
+                r#"
+                frequency = "yearly"
+                yeardays = [1]
+                content = "Happy new year"
+            "#,
+            )));
+
+            assert_eq!(Recurrence::Yearly(vec![Yearday(1)]), event.recurrence);
+        }
+
+        #[test]
+        fn yearly_empty_yeardays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "yearly"
+                content = "Happy new year"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn yearly_weekdays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "yearly"
+                weekdays = ["Monday"]
+                content = "Happy new year"
+            "#,
+            )));
+        }
+
+        #[test]
+        fn yearly_monthdays() {
+            assert_err!(Event::try_from(&block(
+                r#"
+                frequency = "yearly"
+                monthdays = [1]
+                content = "Happy new year"
+            "#,
+            )));
         }
     }
 }
