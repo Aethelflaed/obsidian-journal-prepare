f9bf8551c2f7a28b7077d65518a898ed39605d05
Jaffa Planquart <jaffa@planquart.fr>
2026-02-03 17:02:25 +0100

    wip: Add base for looking up birthdays in notes

---
+++ birthdays/src/main.rs
@@ -0,0 +1,68 @@
+use anyhow::Result;
+use grep::{
+    regex::RegexMatcher,
+    searcher::{BinaryDetection, Searcher, SearcherBuilder, Sink, SinkError, SinkMatch},
+};
+use std::path::Path;
+use walkdir::WalkDir;
+
+struct Detector<'a> {
+    path: &'a Path,
+    detected: bool,
+}
+
+impl<'a> Detector<'a> {
+    fn new(path: &'a Path) -> Detector<'a> {
+        Self {
+            path,
+            detected: false,
+        }
+    }
+}
+
+#[derive(Debug, derive_more::Display, derive_more::Error)]
+#[display("Error searching")]
+pub struct Error;
+
+impl SinkError for Error {
+    fn error_message<T: std::fmt::Display>(_message: T) -> Self {
+        Self
+    }
+}
+
+impl Sink for Detector<'_> {
+    type Error = Error;
+
+    fn matched(&mut self, _searcher: &Searcher, _mat: &SinkMatch<'_>) -> Result<bool, Self::Error> {
+        if !self.detected {
+            println!("{}", self.path.display());
+        }
+        self.detected = true;
+        Ok(true)
+    }
+}
+
+fn main() -> Result<()> {
+    let pattern = "^birthday: \\d{4}-\\d{2}-\\d{2}";
+    let matcher = RegexMatcher::new_line_matcher(pattern)?;
+    let mut searcher = SearcherBuilder::new()
+        .binary_detection(BinaryDetection::quit(b'\x00'))
+        .line_number(false)
+        .build();
+
+    for result in WalkDir::new("journal") {
+        let dent = match result {
+            Ok(dent) => dent,
+            Err(err) => {
+                eprintln!("{}", err);
+                continue;
+            }
+        };
+        if !dent.file_type().is_file() {
+            continue;
+        }
+        let sink = Detector::new(dent.path());
+        searcher.search_path(&matcher, dent.path(), sink)?;
+    }
+    Ok(())
+}
